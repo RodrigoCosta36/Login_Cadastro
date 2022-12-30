42cf07706b3235dccf4e1c2fbf5bfa4cc4a75381
RodrigoCosta36 <95256390+RodrigoCosta36@users.noreply.github.com>
2022-12-30 16:52:44 -0300

    New Project

---
+++ app.js
@@ -0,0 +1,25 @@
+function showHide() {
+    const password = document.getElementById('password');
+    const icon = document.getElementById('icon');
+
+
+    if (password.type === 'password') {
+        password.setAttribute('type', 'text');
+        icon.classList.add('hide')
+    } else {
+        password.setAttribute('type', 'password');
+        icon.classList.remove('hide')
+    }
+}
+
+function entar() {
+    var login = document.getElementById('user').value;
+    var senha = document.getElementById('password').value;
+
+    if (login == 'root' || '000997' && senha == 'root') {
+        alert('Logado Com Sucesso !!');
+        location.href = "https://supernintendo-rodrigo.netlify.app";
+    } else {
+        alert('Usuário ou Senha Inválido, Preencha os Campos.');
+    }
+}
