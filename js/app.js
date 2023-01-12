520b0471765c6066f699f5fd9389777f0432d6a2
RodrigoCosta36 <95256390+RodrigoCosta36@users.noreply.github.com>
2023-01-12 00:34:47 -0300

    correções de Bugs e nova Atualização

--- js/app.js
+++ js/app.js
@@ -178,29 +178,44 @@ class Produto {
 
 var produtos = new Produto();
 
+document.getElementById('exportCSV').addEventListener('click', function () {
+    var table2excel = new Table2Excel();
+    table2excel.export(document.getElementById('export'))
+});
 
-const tableRows = document.querySelectorAll('tr');
-const exportBtn = document.querySelector('[data-js="export-table-btn"]');
 
-const getCellsText = ({ textContent }) => textContent
 
-const getStringWithCommas = ({ cells }) => Array.from(cells)
-    .map(getCellsText)
-    .join(',')
 
-const createCSVString = () => Array.from(tableRows)
-    .map(getStringWithCommas)
-    .join('\n')
 
-const setCSVDownload = CSVString => {
-    const CSVURI = `data:text/csvcharset=utf-8,${encodeURIComponent()}`
-    exportBtn.setAttribute('href', CSVURI)
-    exportBtn.setAttribute('download', 'relatorio.csv')
-}
 
-const exportTable = () => {
-    const CSVString = createCSVString()
-    setCSVDownload(CSVString)
-}
 
-exportBtn.addEventListener('click', exportTable)
+
+
+
+
+
+// const tableRows = document.querySelectorAll('tr');
+// const exportBtn = document.querySelector('[data-js="export-table-btn"]');
+
+// const getCellsText = ({ textContent }) => textContent
+
+// const getStringWithCommas = ({ cells }) => Array.from(cells)
+//     .map(getCellsText)
+//     .join(',')
+
+// const createCSVString = () => Array.from(tableRows)
+//     .map(getStringWithCommas)
+//     .join('\n')
+
+// const setCSVDownload = CSVString => {
+//     const CSVURI = `data:text/csvcharset=utf-8,${encodeURIComponent()}`
+//     exportBtn.setAttribute('href', CSVURI)
+//     exportBtn.setAttribute('download', 'relatorio.csv')
+// }
+
+// const exportTable = () => {
+//     const CSVString = createCSVString()
+//     setCSVDownload(CSVString)
+// }
+
+// exportBtn.addEventListener('click', exportTable)
