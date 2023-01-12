6a2af3eab1bff68896d078d668cce5f8dc7d0202
RodrigoCosta36 <95256390+RodrigoCosta36@users.noreply.github.com>
2023-01-11 23:51:47 -0300

    att

--- js/app.js
+++ js/app.js
@@ -177,3 +177,30 @@ class Produto {
 };
 
 var produtos = new Produto();
+
+
+const tableRows = document.querySelectorAll('tr');
+const exportBtn = document.querySelector('[data-js="export-table-btn"]');
+
+const getCellsText = ({ textContent }) => textContent
+
+const getStringWithCommas = ({ cells }) => Array.from(cells)
+    .map(getCellsText)
+    .join(',')
+
+const createCSVString = () => Array.from(tableRows)
+    .map(getStringWithCommas)
+    .join('\n')
+
+const setCSVDownload = CSVString => {
+    const CSVURI = `data:text/csvcharset=utf-8,${encodeURIComponent()}`
+    exportBtn.setAttribute('href', CSVURI)
+    exportBtn.setAttribute('download', 'relatorio.csv')
+}
+
+const exportTable = () => {
+    const CSVString = createCSVString()
+    setCSVDownload(CSVString)
+}
+
+exportBtn.addEventListener('click', exportTable)
