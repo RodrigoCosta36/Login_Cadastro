845f132b430373fd5276708a902d1e09fd43432d
RodrigoCosta36 <95256390+RodrigoCosta36@users.noreply.github.com>
2023-01-16 19:18:13 -0300

    Update scriptLogin.js

--- js/scriptLogin.js
+++ js/scriptLogin.js
@@ -31,7 +31,6 @@ function entrar() {
                 //window.location.href = "https://conferenciapa.netlify.app/conferencia.html";
                 let token = Math.random().toString(32).substr(2) + Math.random().toString(32).substr(2) + Math.random().toString(32).substr(2);
                 localStorage.setItem('token', token);
-                localStorage.setItem('usuarioLogado', JSON.stringify(usuario));
             } else {
                 alert('Campo usuário ou senha incoreto.');
             }
